f2b290ea3c19130c4f5474ba53434f6ea789d9cc
Boa Ho Man <boa.homan@gmail.com>
2016-12-02 15:10:20 +0800

    Fade in/out

--- source/javascripts/init_headroom.js
+++ source/javascripts/init_headroom.js
@@ -12,23 +12,29 @@ $(document).ready(function() {
     }
   });
 
-//   $("div.pagination").headroom({
-//     "offset": 205,
-//     "tolerance": 5,
-//     "classes": {
-//       "initial": "animated",
-//       "pinned": "slideInRight",
-//       "unpinned": "slideOutRight"
-//     }
-//   });
+  $("div.pagination").headroom({
+    "offset": 205,
+    "tolerance": {
+      "up": 5,
+      "down": 0
+    },
+    "classes": {
+      "initial": "animated",
+      "pinned": "fadeIn",
+      "unpinned": "fadeOut"
+    }
+  });
 
-//   $("div.tags").headroom({
-//     "offset": 205,
-//     "tolerance": 5,
-//     "classes": {
-//       "initial": "animated",
-//       "pinned": "slideInRight",
-//       "unpinned": "slideOutRight"
-//     }
-//   });
+  $("div.tags").headroom({
+    "offset": 205,
+    "tolerance": {
+      "up": 5,
+      "down": 0
+    },
+    "classes": {
+      "initial": "animated",
+      "pinned": "fadeIn",
+      "unpinned": "fadeOut"
+    }
+  });
 });
