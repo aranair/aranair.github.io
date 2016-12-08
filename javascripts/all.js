51118a093ae483c659edf3b25757703606a0917e
Boa Ho Man <boa.homan@gmail.com>
2016-12-08 22:14:59 +0800

    Automated commit at 2016-12-08 14:14:58 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -10854,18 +10854,18 @@ $(document).ready(function() {
     }
   });
 
-  $("div.pagination").headroom({
-    "offset": 205,
-    "tolerance": {
-      "up": 5,
-      "down": 0
-    },
-    "classes": {
-      "initial": "animated",
-      "pinned": "fadeIn",
-      "unpinned": "fadeOut"
-    }
-  });
+  // $("div.pagination").headroom({
+  //   "offset": 205,
+  //   "tolerance": {
+  //     "up": 5,
+  //     "down": 0
+  //   },
+  //   "classes": {
+  //     "initial": "animated",
+  //     "pinned": "fadeIn",
+  //     "unpinned": "fadeOut"
+  //   }
+  // });
 
   $("div.tags").headroom({
     "offset": 205,
