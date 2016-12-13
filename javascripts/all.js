4b2c09789f909d7811c6ec0c5f590cde622053c0
Boa Ho Man <boa.homan@gmail.com>
2016-12-13 22:44:52 +0800

    Automated commit at 2016-12-13 14:44:52 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/all.js
+++ javascripts/all.js
@@ -10836,7 +10836,7 @@ return jQuery;
 }(window.jQuery);
 $(document).ready(function() {
   $(".index-title__job").typed({
-    strings: ["Software Engineer at Pocketmath."],
+    strings: ["Software Engineer at <a href='https://www.pocketmath.com' target='_blank'>Pocketmath</a>."],
     typeSpeed: 0.1
   });
 });
--- javascripts/init_typed.js
+++ javascripts/init_typed.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".index-title__job").typed({
-    strings: ["Software Engineer at Pocketmath."],
+    strings: ["Software Engineer at <a href='https://www.pocketmath.com' target='_blank'>Pocketmath</a>."],
     typeSpeed: 0.1
   });
 });
