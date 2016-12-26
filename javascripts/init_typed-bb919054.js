0395a0743a2513adb41559d8ccf2b82604e75cf6
Boa Ho Man <boa.homan@gmail.com>
2016-12-26 10:39:03 +0800

    Automated commit at 2016-12-26 02:39:03 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/init_typed-bb919054.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".index-title__job").typed({strings:["Software Engineer at <a href='https://www.pocketmath.com' target='_blank'>Pocketmath</a>."],typeSpeed:.1})});
--- javascripts/init_typed.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".index-title__job").typed({
-    strings: ["Software Engineer at <a href='https://www.pocketmath.com' target='_blank'>Pocketmath</a>."],
-    typeSpeed: 0.1
-  });
-});
