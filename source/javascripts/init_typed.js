6365ff42145df3fb0862fb5ae3a15ee46762ccce
Homan <homan@tulip.co>
2017-09-01 09:11:05 -0400

    Update Bio

--- source/javascripts/init_typed.js
+++ source/javascripts/init_typed.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".index-title__job").typed({
-    strings: ["Software Engineer at <a href='https://www.pocketmath.com' target='_blank'>Pocketmath</a>."],
+    strings: ["Software Engineer at <a href='https://tulip.co' target='_blank'>Tulip</a>."],
     typeSpeed: 0.1
   });
 });
